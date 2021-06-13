0bdb4bfe505c43d6e98c6cbbd322937b3031187c
thenabeelahmad <nabeelahmad4644@gmail.com>
2021-06-13 11:25:41 +0530

    MVC Db Connection

---
+++ MVC-ConnectionDemo/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient; 
+
+
+namespace MVC_ConnectionDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        DBManager dm = new DBManager(); 
+
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection frm)
+        {
+            string tf1 = frm[0];
+            string tf2 = frm[1];
+            string q = "insert into tbl_testdb values('" + tf1 + "','" + tf2 + "')";
+            bool res = dm.InsertUpdateDelete(q);
+            if (res == true)
+            {
+                ViewBag.confirm = "Record Inserted Successfully";
+            }
+            else
+            {
+                ViewBag.confirm = "Database Error or Query Error";
+            }
+
+            
+            return View();
+
+        }
+
+        public ActionResult DisplayRecords()
+        {
+            string q = "select * from tbl_testdb";
+            DataTable dt = new DataTable();
+            dt = dm.ReadBulkData(q);
+            string tbl = "<table class='table table-striped'>";
+            int i;
+            tbl += "<tr>";
+            tbl += "<th>TestField 1 </th><th>TestField 2</th>";
+            tbl += "</tr>";
+
+            for (i = 0; i < dt.Rows.Count; i++)
+            {
+                tbl += "<tr>";
+                tbl += "<td>" + dt.Rows[i][0] + "</td>";
+                tbl += "<td>" + dt.Rows[i][1] + "</td>";
+                tbl += "</tr>";
+
+
+            }
+            tbl += "</table>";
+            ViewBag.table = tbl;
+
+
+            return View();
+        }
+       
+
+
+    }
+}
---
+++ MVC-ConnectionDemo/DBManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient; 
+
+
+namespace MVC_ConnectionDemo
+{
+    public class DBManager
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-8UBAOPE\\NABEELEXPRESS;Initial Catalog=TestingDb;Integrated Security=True");
+
+        public bool InsertUpdateDelete(string command)
+        {
+            SqlCommand cmd = new SqlCommand(command, con);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            int j = cmd.ExecuteNonQuery();
+            if (j > 0)
+                return true;
+            else
+                return false;
+
+        }
+        public DataTable ReadBulkData(string command)
+        {
+            SqlDataAdapter sa = new SqlDataAdapter(command, con);
+            DataTable datatbl = new DataTable();
+            sa.Fill(datatbl);
+            return datatbl;
+
+
+        }
+
+    }
+}
